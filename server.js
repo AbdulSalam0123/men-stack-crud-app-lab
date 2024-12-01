d700dd900c9ec27c4a7ecb8174236daf93bbee87
Abdulsalam20000 <20193925@stu.uob.edu.bh>
2024-12-01 09:05:10 +0300

    done

---
+++ server.js
@@ -0,0 +1,72 @@
+const Food = require("./models/food");
+require('dotenv').config();
+const express = require("express");
+const mongoose = require('mongoose');
+const app= express();
+const port= 3000;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const methodOverride = require('method-override'); 
+app.use(methodOverride('_method'));
+//Connecting mongoose with the URI in .env
+mongoose.connect(process.env.MONGODB_URI);
+
+// Checking if the connection is on
+mongoose.connection.on('connected', ()=>
+{
+    console.log("Connected to MongoDB:" ,mongoose.connection.name);
+});
+
+app.get("/foods/new", (req,res)=>
+{
+    res.render("new");
+});
+
+//Curd Add
+app.post("/foods", async (req,res)=>
+{
+    await Food.create(req.body);
+    res.redirect("/foods");
+});
+
+app.get("/foods", async (req,res) =>
+{
+    const foods = await Food.find();
+    res.render("foods/index.ejs", { foods });
+})
+
+app.get('/foods/:id', async (req, res) => 
+{
+    const food = await Food.findById(req.params.id);
+    res.render('show', { food });
+});
+
+app.get("/foods/:foodId/edit", async (req, res) =>
+{
+  const food = await Food.findById(req.params.fruitId);
+  res.render("foods/edit.ejs", { food });
+})
+
+app.put('/foods/:id', async (req, res) => 
+{
+    const food = await Food.findByIdAndUpdate(req.params.id, req.body, { new: true }); 
+    res.redirect(`/foods/${food._id}`);
+});
+
+app.delete('/foods/:id', async (req, res) => 
+{
+    await Food.findByIdAndDelete(req.params.id); 
+    res.redirect('/foods'); 
+});
+
+app.get("/", (req,res)=>
+{
+    res.render('index');
+});
+
+app.listen(port, (req,res)=>
+{
+    console.log("App is listening on port:", port);
+});
